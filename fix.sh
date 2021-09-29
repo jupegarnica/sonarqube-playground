ea4d97e98dd667144a3c36b6e593561bddb23330
Jupegarnica <juan@geekshubs.com>
2021-09-29 13:47:24 +0200

    use oficial sonarqube image

--- fix.sh
+++ fix.sh
@@ -1,2 +1,5 @@
 # https://github.com/bitnami/bitnami-docker-sonarqube/issues/26#issuecomment-651101533
-sudo sysctl -w vm.max_map_count=262144
+sudo sysctl -w vm.max_map_count=524288
+sudo sysctl -w fs.file-max=131072
+sudo ulimit -n 131072
+sudo ulimit -u 8192
