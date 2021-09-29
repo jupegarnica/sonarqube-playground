8a5195d579b7883d88ccd45fec3612a5da99e371
Jupegarnica <juan@geekshubs.com>
2021-09-29 16:12:28 +0200

    move scan to docker-compose

--- scan.sh
+++ scan.sh
@@ -1,10 +1,17 @@
-export TOKEN=ec380e57f34b57ba44d1f020b070af7024a0cac5
-export YOUR_REPO=/home/juanperez/dev/sonarqube/app
+export TOKEN=34eaef7a15e57cf4794100a4a31ab2dcf9340d48
+export SRC=app
 export SONARQUBE_URL=http://localhost:9000
 
-docker run \
-    --rm \
-    -e SONAR_HOST_URL="${SONARQUBE_URL}" \
-    -e SONAR_LOGIN="${TOKEN}" \
-    -v "${YOUR_REPO}:/usr/src" \
-    sonarsource/sonar-scanner-cli
+# docker run \
+#     --rm \
+#     -e SONAR_HOST_URL="${SONARQUBE_URL}" \
+#     -e SONAR_LOGIN="${TOKEN}" \
+#     -v "${SRC}:/usr/src" \
+#     sonarsource/sonar-scanner-cli
+
+
+sonar-scanner \
+  -Dsonar.projectKey=test \
+  -Dsonar.sources=$SRC \
+  -Dsonar.host.url=$SONARQUBE_URL \
+  -Dsonar.login=$TOKEN
